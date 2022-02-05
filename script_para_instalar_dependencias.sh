f42b1947d39b0cf014c7118c9497f981c11eee75
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2022-02-05 19:16:57 -0400

    Create script_para_instalar_dependencias.sh

---
+++ script_para_instalar_dependencias.sh
@@ -0,0 +1,22 @@
+#/bash/bin
+
+sudo apt update;
+sudo apt upgrade;
+
+
+sudo apt install python3;
+sudo apt install pip3;
+
+sudo pip3 install modlamp;
+sudo pip3 install sklearn;
+
+
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0;
+sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0;
+
+
+pip3 install pycairo;
+pip3 install PyGObject;
+
+sudo apt update;
+sudo apt upgrade;
