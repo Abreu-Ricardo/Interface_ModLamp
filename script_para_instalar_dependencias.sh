0252d1ecd0fd2515a94f2bd7eed10628719e027e
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2024-01-24 21:32:33 -0400

    arrumei algumas dependências erradas

--- script_para_instalar_dependencias.sh
+++ script_para_instalar_dependencias.sh
@@ -1,22 +1,22 @@
-#!/bash/bin
+#!/bin/bash
 
 sudo apt update;
 sudo apt upgrade;
 
 
 sudo apt install python3;
-sudo apt install pip3;
+apt install pip3;
 
 sudo pip3 install modlamp;
-sudo pip3 install sklearn;
+pip3 install scikit-learn;
 
 
 sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0;
 sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0;
 
 
-pip3 install pycairo;
-pip3 install PyGObject;
+install pycairo;
+install PyGObject;
 
 sudo apt update;
 sudo apt upgrade;
